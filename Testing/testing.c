f3b5712b28fd83dc452a9c7d7251cb064a8ca87f
Raphael Preston <kingoftheworld4000@gmail.com>
2017-07-30 00:45:29 -0600

    Updated tests

--- Testing/testing.c
+++ Testing/testing.c
@@ -16,24 +16,67 @@ int main(int argc, char * argv[])
 	//}
 	//// fileToXML("C:/Users/IAMFRANK/Documents/FB Testing/messages.htm", "C:/Users/IAMFRANK/Documents/FB Testing/output.txt");
 
+	////////////////// runs through all the characters and does nothing to them
+
 	FILE * messagesP;
-	messagesP = fopen("C:/Users/IAMFRANK/Documents/FB Testing/messages.htm", "r");
-	FILE * outputP;
-	outputP = fopen("C:/Users/IAMFRANK/Documents/FB Testing/output.txt", "w");
 	
-
-
+	FILE * outputP;
+	FILE * testing;
+	testing = fopen("C:/Users/IAMFRANK/Documents/FB Testing/bigtesting.txt", "w");
+	
 	struct timeb start, end;
 	double diff;
-	ftime(&start);
-	char c;
-	while ((c = fgetc(messagesP)) != EOF) {
-		fprintf(outputP, "%c", c);
+	for (int i = 0; i < 30; i++) {
+		printf("%i\t", i);
+		outputP = fopen("C:/Users/IAMFRANK/Documents/FB Testing/output.txt", "w");
+		messagesP = fopen("C:/Users/IAMFRANK/Documents/FB Testing/messages.htm", "r");
+		ftime(&start);
+		char c;
+		while ((c = fgetc(messagesP)) != EOF) {
+			fprintf(outputP, "%c", c);
+		}
+		ftime(&end);
+		diff = (double)(1000.0 * (end.time - start.time) + (end.millitm - start.millitm));
+		printf("%.0f\n", diff);
+		fclose(messagesP);
+		fclose(outputP);
 	}
 
-	ftime(&end);
-	diff = (double)(1000.0 * (end.time - start.time) + (end.millitm - start.millitm));
-	printf("%.0f\n", diff);
+	fclose(testing);
+	//////////////^^^^^^////////////
+
+	//FILE * inputP;
+	//FILE * outputP;
+	//FILE * testing;
+	//testing = fopen("C:/Users/IAMFRANK/Documents/FB Testing/bigtesting.txt", "w");
+	//double total;
+	//double average;
+	//struct timeb start, end;
+	//int fileNum;
+	//for (fileNum = 1; fileNum <= 83380072; fileNum += 656536) {	//executes for every file
+	//	char fileName[47 + 1];
+	//	snprintf(fileName, sizeof(fileName), "C:/Users/IAMFRANK/Documents/FB Testing/%i", fileNum);
+	//	total = 0.0;
+	//	//test thirty times to get average
+	//	for (int i = 0; i < 30; i++) {
+	//		inputP = fopen(fileName, "r");
+	//		outputP = fopen("C:/Users/IAMFRANK/Documents/FB Testing/output.txt", "w");
+	//		if (inputP == NULL || outputP == NULL) {
+	//			printf("ERROR FILE %i NOT FOUND", fileNum);
+	//			return 50;
+	//		}
+	//		ftime(&start);
+	//		fileToXML(inputP, outputP);
+	//		ftime(&end);
+	//		total += (double)(1000.0 * (end.time - start.time) + (end.millitm - start.millitm));
+	//		fclose(inputP);
+	//		fclose(outputP);
+	//	}
+	//	average = total / 30.0;
+	//	fprintf("%i\t%.3f\n", fileNum, average);
+	//}
+	//fclose(testing);
+	/////////////////////////////////
 
 
 	//int charsMax;
